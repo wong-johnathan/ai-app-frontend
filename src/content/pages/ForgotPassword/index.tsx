2d9ac523a613692c894d4c3b78c7922516960a2a
wong-johnathan <77264935+wong-johnathan@users.noreply.github.com>
2023-08-30 10:10:13 +0800

    first commit

---
+++ src/content/pages/ForgotPassword/index.tsx
@@ -0,0 +1,88 @@
+import {
+  Box,
+  Card,
+  Typography,
+  Container,
+  Divider,
+  Button,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  styled,
+  Stack
+} from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+import { Email } from '@mui/icons-material';
+import Link from '../Components/Link';
+
+const MainContent = styled(Box)(
+  () => `
+    height: 100%;
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+);
+
+const OutlinedInputWrapper = styled(OutlinedInput)(
+  ({ theme }) => `
+    background-color: ${theme.colors.alpha.white[100]};
+`
+);
+
+function ForgotPassword() {
+  return (
+    <>
+      <Helmet>
+        <title>Forgot Password</title>
+      </Helmet>
+      <MainContent>
+        <Container maxWidth="md">
+          <Box textAlign="center">
+            <Typography variant="h2" sx={{ my: 2 }}>
+              Forgot Password
+            </Typography>
+          </Box>
+          <Container maxWidth="xs">
+            <FormControl variant="outlined" fullWidth>
+              <OutlinedInputWrapper
+                type="email"
+                placeholder="Email"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Email />
+                  </InputAdornment>
+                }
+                sx={{ mb: 2, height: '50px' }}
+              />
+              <OutlinedInputWrapper
+                type="email"
+                placeholder="Confirm Email"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Email />
+                  </InputAdornment>
+                }
+                sx={{ mb: 2, height: '50px' }}
+              />
+              <Button variant="outlined" sx={{ mb: 2, height: '50px' }}>
+                Submit
+              </Button>
+            </FormControl>
+            <Divider sx={{ mb: 1 }} />
+            <Stack direction={'row'} justifyContent="flex-start">
+              <Link href="/sign-in" variant="body2">
+                Remember your password? Sign In
+              </Link>
+            </Stack>
+          </Container>
+        </Container>
+      </MainContent>
+    </>
+  );
+}
+
+export default ForgotPassword;
---
+++ src/content/pages/Signin/index.tsx
@@ -0,0 +1,91 @@
+import {
+  Box,
+  Card,
+  Typography,
+  Container,
+  Divider,
+  Button,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  styled,
+  Stack
+} from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+import { Email, Password } from '@mui/icons-material';
+import Link from '../Components/Link';
+
+const MainContent = styled(Box)(
+  () => `
+    height: 100%;
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+);
+
+const OutlinedInputWrapper = styled(OutlinedInput)(
+  ({ theme }) => `
+    background-color: ${theme.colors.alpha.white[100]};
+`
+);
+
+function Signin() {
+  return (
+    <>
+      <Helmet>
+        <title>Signin</title>
+      </Helmet>
+      <MainContent>
+        <Container maxWidth="md">
+          <Box textAlign="center">
+            <Typography variant="h2" sx={{ my: 2 }}>
+              Sign in
+            </Typography>
+          </Box>
+          <Container maxWidth="xs">
+            <FormControl variant="outlined" fullWidth>
+              <OutlinedInputWrapper
+                type="text"
+                placeholder="Email"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Email />
+                  </InputAdornment>
+                }
+                sx={{ mb: 2, height: '50px' }}
+              />
+              <OutlinedInputWrapper
+                type="password"
+                placeholder="Password"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Password />
+                  </InputAdornment>
+                }
+                sx={{ mb: 2, height: '50px' }}
+              />
+              <Button variant="outlined" sx={{ mb: 2, height: '50px' }}>
+                Sign In
+              </Button>
+            </FormControl>
+            <Divider sx={{ mb: 1 }} />
+            <Stack direction={'row'} justifyContent="space-between">
+              <Link href="/forgot-password" variant="body2">
+                Forgot password?
+              </Link>
+              <Link href="/sign-up" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Stack>
+          </Container>
+        </Container>
+      </MainContent>
+    </>
+  );
+}
+
+export default Signin;
