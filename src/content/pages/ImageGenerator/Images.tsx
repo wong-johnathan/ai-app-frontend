41384082a484e4d2f59625cda5cb3a6f2bcde73f
wong-johnathan <wong.johnathanwh@gmail.com>
2023-09-01 21:26:34 +0800

    responsive image list

--- src/content/pages/ImageGenerator/Images.tsx
+++ src/content/pages/ImageGenerator/Images.tsx
@@ -4,23 +4,27 @@ import {
   CardHeader,
   Divider,
   ImageList,
-  ImageListItem
+  ImageListItem,
+  useMediaQuery,
+  useTheme
 } from '@mui/material';
 
-import Dummy from 'dummyjs'
+import Dummy from 'dummyjs';
 const Images = () => {
+  const theme = useTheme();
+  const matchDownMd = useMediaQuery(theme.breakpoints.up('md'));
+  console.log(matchDownMd)
   return (
     <Card>
       <CardHeader title="Generated Images" />
       <Divider />
       <CardContent>
-        <ImageList cols={4} gap={8}>
+        <ImageList cols={matchDownMd ? 4 : 2} gap={8}>
           {[...new Array(20)].map((arr, index) => (
             <ImageListItem key={index}>
               <img
                 style={{ cursor: 'pointer' }}
                 src={`${Dummy.img('200x200')}`}
-                width="100%"
                 alt={Dummy.text(10)}
                 loading="lazy"
               />
